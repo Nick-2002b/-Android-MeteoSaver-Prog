ac78d5f81c9512126615b093f3acbba9b6d4069f
nick_2002b <siriusdark95@gmail.com>
2025-08-28 17:32:51 +0200

    Create Weather data class

---
+++ domain/src/main/java/com/unibo/domain/model/Weather.kt
@@ -0,0 +1,11 @@
+package com.unibo.domain.model
+
+data class Weather (
+    val cityName: String,
+    val icon: String,
+    val weatherDescription: String,
+    val temperature: Double,
+    val humidity: Double,
+    val windSpeed: Double,
+    val feelsLike: Double,
+)

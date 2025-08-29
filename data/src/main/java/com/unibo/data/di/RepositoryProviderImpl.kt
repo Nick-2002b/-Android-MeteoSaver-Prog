a2c865e6418ec1e0b860adf06a718b3acef8bb23
nick_2002b <siriusdark95@gmail.com>
2025-08-29 18:33:43 +0200

    Provide WeatherRepository implementation using Retrofit

--- data/src/main/java/com/unibo/data/di/RepositoryProviderImpl.kt
+++ data/src/main/java/com/unibo/data/di/RepositoryProviderImpl.kt
@@ -1,8 +1,11 @@
 package com.unibo.data.di
 
 import com.unibo.data.remote.RetrofitClient
+import com.unibo.data.repository.WeatherRepositoryImpl
 import com.unibo.domain.di.RepositoryProvider
+import com.unibo.domain.repository.WeatherRepository
 
 class RepositoryProviderImpl: RepositoryProvider {
     private val retrofitClient = RetrofitClient()
+    override val weatherRepository: WeatherRepository = WeatherRepositoryImpl(weatherApi = retrofitClient.weatherApi)
 }

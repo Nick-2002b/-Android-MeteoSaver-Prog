edd8b078ce9b82f641f86fba8dfbd15c55050b3d
nick_2002b <siriusdark95@gmail.com>
2025-08-30 18:16:29 +0200

    Implement LocalDB

--- data/src/main/java/com/unibo/data/local/db/AppDatabase.kt
+++ data/src/main/java/com/unibo/data/local/db/AppDatabase.kt
@@ -1,5 +1,31 @@
 package com.unibo.data.local.db
 
-class AppDatabase {
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+import com.unibo.data.local.dao.WeatherDao
+import com.unibo.data.local.entities.WeatherLocalModel
 
+@Database(entities = [WeatherLocalModel::class], version = 1)
+abstract class AppDatabase : RoomDatabase() {
+
+    abstract fun weatherDao(): WeatherDao
+
+    companion object {
+        @Volatile
+        private var INSTANCE: AppDatabase? = null
+
+        fun getInstance(context: Context): AppDatabase {
+            return INSTANCE ?: synchronized(this) {
+                val instance = Room.databaseBuilder(
+                    context.applicationContext,
+                    AppDatabase::class.java,
+                    "meteosaver_database"
+                ).build()
+                INSTANCE = instance
+                instance
+            }
+        }
+    }
 }

e66bcd4033756edd5d560ed33322cc1d876f5a79
nick_2002b <siriusdark95@gmail.com>
2025-08-30 17:16:29 +0200

    Create Main model for Api Response

---
+++ data/src/main/java/com/unibo/data/remote/models/Main.kt
@@ -0,0 +1,11 @@
+package com.unibo.data.remote.models
+
+import com.squareup.moshi.Json
+import com.squareup.moshi.JsonClass
+
+@JsonClass(generateAdapter = true)
+data class Main(
+    @Json(name = "temp") val temperature: Double? = null,
+    @Json(name = "humidity") val humidity: Double? = null,
+    @Json(name = "feels_like") val feelsLike: Double? = null,
+)

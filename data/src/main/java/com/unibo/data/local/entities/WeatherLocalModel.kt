ad87bf475b986efcab9c212e78da1cbedf0a0d61
nick_2002b <siriusdark95@gmail.com>
2025-08-30 18:00:20 +0200

    Create LocalModel for room db

---
+++ data/src/main/java/com/unibo/data/local/entities/WeatherLocalModel.kt
@@ -0,0 +1,15 @@
+package com.unibo.data.local.entities
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "weather_locations")
+data class WeatherLocalModel(
+    @PrimaryKey val cityName: String,
+    val icon: String,
+    val weatherDescription: String,
+    val temperature: Double,
+    val humidity: Double,
+    val windSpeed: Double,
+    val feelsLike: Double,
+)

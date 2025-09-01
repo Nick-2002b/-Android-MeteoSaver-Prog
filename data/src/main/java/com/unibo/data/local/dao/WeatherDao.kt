3720cc6be89d276365731c4964e3cbb97634dd59
nick_2002b <siriusdark95@gmail.com>
2025-09-01 11:25:36 +0200

    Fix type mismatch

--- data/src/main/java/com/unibo/data/local/dao/WeatherDao.kt
+++ data/src/main/java/com/unibo/data/local/dao/WeatherDao.kt
@@ -4,12 +4,13 @@ import androidx.room.Dao
 import androidx.room.Insert
 import androidx.room.OnConflictStrategy
 import androidx.room.Query
+import com.unibo.data.local.entities.WeatherLocalModel
 import com.unibo.domain.model.Weather
 
 @Dao
 interface WeatherDao {
     @Insert(onConflict = OnConflictStrategy.REPLACE)
-    suspend fun insertWeather(weather: Weather)
+    suspend fun insertWeather(weather: WeatherLocalModel)
 
     @Query("SELECT * FROM weather_locations ORDER BY lastUpdate DESC")
     suspend fun getAllWeather(): List<Weather>

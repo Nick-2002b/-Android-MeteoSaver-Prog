9e22225173d85b56d30643fdcee2ba4cd9c0c2b0
nick_2002b <siriusdark95@gmail.com>
2025-09-01 23:01:39 +0200

    Add Usecases to the provider

--- domain/src/main/java/com/unibo/domain/di/UseCaseProvider.kt
+++ domain/src/main/java/com/unibo/domain/di/UseCaseProvider.kt
@@ -1,8 +1,24 @@
 package com.unibo.domain.di
 
+import com.unibo.domain.usecases.FetchRemoteWeatherUseCase
+import com.unibo.domain.usecases.FetchRemoteWeatherUseCaseImpl
+import com.unibo.domain.usecases.GetWeatherListUseCase
+import com.unibo.domain.usecases.GetWeatherListUseCaseImpl
+
 object UseCaseProvider {
+    lateinit var fetchRemoteWeatherUseCase: FetchRemoteWeatherUseCase
+    lateinit var getWeatherListUseCase: GetWeatherListUseCase
+
     fun setup(
         repositoryProvider: RepositoryProvider,
     ){
+        fetchRemoteWeatherUseCase = FetchRemoteWeatherUseCaseImpl(
+            repository = repositoryProvider.weatherRepository
+        )
+
+        getWeatherListUseCase = GetWeatherListUseCaseImpl(
+            repository = repositoryProvider.weatherRepository
+        )
+
     }
 }

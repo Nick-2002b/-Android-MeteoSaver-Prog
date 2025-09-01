6328c5dc8f9fe817cfa2fef908b1ea21392890e4
nick_2002b <siriusdark95@gmail.com>
2025-09-01 11:16:19 +0200

    Implement getRemoteWeatherByCity & toEntity functions

--- data/src/main/java/com/unibo/data/repository/WeatherRepositoryImpl.kt
+++ data/src/main/java/com/unibo/data/repository/WeatherRepositoryImpl.kt
@@ -1,13 +1,40 @@
 package com.unibo.data.repository
 
+import android.content.Context
+import com.unibo.data.local.db.AppDatabase
+import com.unibo.data.local.entities.WeatherLocalModel
 import com.unibo.data.remote.WeatherApiService
+import com.unibo.data.remote.models.ApiResponse
 import com.unibo.domain.model.Weather
 import com.unibo.domain.repository.WeatherRepository
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.launch
 
-class WeatherRepositoryImpl(private val weatherApiService: WeatherApiService) : WeatherRepository {
+class WeatherRepositoryImpl(
+    private val weatherApiService: WeatherApiService,
+    context: Context
+) : WeatherRepository {
+
+    private val scope = CoroutineScope(Dispatchers.IO)
+
+    private val _weatherList = MutableStateFlow<List<Weather>>(listOf())
+    override val weatherList: StateFlow<List<Weather>> = _weatherList
+
+    private val weatherDao = AppDatabase.getInstance(context = context).weatherDao()
+
     override suspend fun getRemoteWeatherByCity(cityName: String): Result<Weather> {
-        TODO("Not yet implemented")
+        scope.launch {
+            try {
+                val response = weatherApiService.getWeather(cityName, lang = "IT")
+                weatherDao.insertWeather(response.toEntity())
+            } catch (e: Exception){
+                println("Error during data fetch: ${e.message}")
+            }
+        }
     }
 
     override fun getSavedWeather(): Flow<List<Weather>> {
@@ -16,5 +43,19 @@ class WeatherRepositoryImpl(private val weatherApiService: WeatherApiService) : 
 
     override suspend fun saveWeatherLocation(weather: Weather) {
         TODO("Not yet implemented")
+    }
+
+    // MAPPERS
+    private fun ApiResponse.toEntity(): WeatherLocalModel {
+        return WeatherLocalModel(
+            cityName = this.cityName ?: "Sconosciuta",
+            icon = this.weather?.firstOrNull()?.icon?.let { iconCode -> "https://openweather.site/img/wn/$iconCode.png" }?: "",
+            weatherDescription = this.weather?.firstOrNull()?.description ?: "N/D",
+            temperature = this.main?.temperature ?: 0.0,
+            humidity = this.main?.humidity ?: 0.0,
+            windSpeed = this.wind?.speed ?: 0.0,
+            feelsLike = this.main?.feelsLike ?: 0.0,
+            lastUpdate = System.currentTimeMillis(),
+        )
     }
 }

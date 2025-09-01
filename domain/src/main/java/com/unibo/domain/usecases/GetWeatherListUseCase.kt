71af4310442422ca1861719e7774f081f09ba8eb
nick_2002b <siriusdark95@gmail.com>
2025-09-01 11:51:25 +0200

    Implement UseCase

--- domain/src/main/java/com/unibo/domain/usecases/GetWeatherListUseCase.kt
+++ domain/src/main/java/com/unibo/domain/usecases/GetWeatherListUseCase.kt
@@ -1,4 +1,15 @@
 package com.unibo.domain.usecases
 
-class GetWeatherListUseCase {
+import com.unibo.domain.model.Weather
+import com.unibo.domain.repository.WeatherRepository
+import kotlinx.coroutines.flow.StateFlow
+
+interface GetWeatherListUseCase: () -> StateFlow<List<Weather>>
+
+class GetWeatherListUseCaseImpl(
+    private val repository: WeatherRepository
+): GetWeatherListUseCase {
+    override fun invoke(): StateFlow<List<Weather>> {
+        return repository.weatherList
+    }
 }

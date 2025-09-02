022ef147b1253b14aa6fefa8f8535081b5c37bee
nick_2002b <siriusdark95@gmail.com>
2025-09-02 15:35:25 +0200

    Create new file

---
+++ ui/src/main/java/com/unibo/ui/homepage/WeatherCard.kt
@@ -0,0 +1,12 @@
+package com.unibo.ui.homepage
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import com.unibo.domain.model.Weather
+
+@Composable
+fun WeatherCard (
+    weather: Weather,
+    modifier: Modifier = Modifier
+) {
+}

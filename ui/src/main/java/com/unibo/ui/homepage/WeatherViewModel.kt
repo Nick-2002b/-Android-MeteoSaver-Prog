e7e5efca3a029c9e1fc509e04330ab64f9a07320
nick_2002b <siriusdark95@gmail.com>
2025-09-02 11:38:09 +0200

    Implemented weatherViewModel

--- ui/src/main/java/com/unibo/ui/homepage/WeatherViewModel.kt
+++ ui/src/main/java/com/unibo/ui/homepage/WeatherViewModel.kt
@@ -1,12 +1,42 @@
 package com.unibo.ui.homepage
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.unibo.domain.model.Weather
 import com.unibo.domain.usecases.FetchRemoteWeatherUseCase
 import com.unibo.domain.usecases.GetWeatherListUseCase
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.launch
 
 class WeatherViewModel (
     private val getWeatherListUseCase: GetWeatherListUseCase,
     private val fetchRemoteWeatherUseCase: FetchRemoteWeatherUseCase
 ): ViewModel() {
+
+    private val _weatherList = MutableStateFlow<List<Weather>>(listOf())
+    val weatherList: StateFlow<List<Weather>> = _weatherList
+
+    private val _showLoader = MutableStateFlow(false)
+    val showLoader: StateFlow<Boolean> = _showLoader
+
+    init {
+        observeWeatherList()
+    }
+
+    fun searchCity(cityName: String){
+        fetchRemoteWeatherUseCase.invoke(cityName)
+    }
+
+    private fun observeWeatherList(){
+        viewModelScope.launch {
+            _showLoader.value = true
+
+            getWeatherListUseCase().collect { weatherFromRepo ->
+                _weatherList.value = weatherFromRepo
+                _showLoader.value = false
+            }
+        }
+    }
 
 }

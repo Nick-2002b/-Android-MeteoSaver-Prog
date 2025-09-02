53ce4d327f275df3f99da78929ef1b63f72a11ea
nick_2002b <siriusdark95@gmail.com>
2025-09-02 11:06:28 +0200

    Passing the dependencies of UseCases

--- ui/src/main/java/com/unibo/ui/homepage/WeatherViewModel.kt
+++ ui/src/main/java/com/unibo/ui/homepage/WeatherViewModel.kt
@@ -1,8 +1,12 @@
 package com.unibo.ui.homepage
 
 import androidx.lifecycle.ViewModel
+import com.unibo.domain.usecases.FetchRemoteWeatherUseCase
+import com.unibo.domain.usecases.GetWeatherListUseCase
 
 class WeatherViewModel (
-
+    private val getWeatherListUseCase: GetWeatherListUseCase,
+    private val fetchRemoteWeatherUseCase: FetchRemoteWeatherUseCase
 ): ViewModel() {
+
 }

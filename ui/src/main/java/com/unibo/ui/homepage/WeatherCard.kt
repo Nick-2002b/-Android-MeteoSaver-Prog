30363e82c45c7c01814f39af21fe568327533b26
nick_2002b <siriusdark95@gmail.com>
2025-09-03 18:11:03 +0200

    Create weathercard design

--- ui/src/main/java/com/unibo/ui/homepage/WeatherCard.kt
+++ ui/src/main/java/com/unibo/ui/homepage/WeatherCard.kt
@@ -1,24 +1,28 @@
 package com.unibo.ui.homepage
+import android.R
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+//import androidx.compose.material.icons.filled.KeyboardArrowRight
+import androidx.compose.material.icons.automirrored.filled.KeyboardArrowRight
+import androidx.compose.material.icons.filled.Star
 import androidx.compose.material3.Card
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
-import androidx.compose.ui.draw.shadow
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.res.dimensionResource
-import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
@@ -27,21 +31,52 @@ import androidx.compose.ui.unit.sp
 import com.unibo.domain.model.Weather
 
 @Composable
-fun WeatherCard (
+fun WeatherCard(
     weather: Weather,
     onItemClick: (Weather) -> Unit,
 ) {
-    Card (
+    Card(
         modifier = Modifier
             .fillMaxWidth()
-            .background(color = Color.LightGray)
+            .clip(RoundedCornerShape(8.dp))
             .clickable { onItemClick(weather) }
-        ,shape = RoundedCornerShape(8.dp)
     ) {
-        Text(
-            text = "Test: ${weather.cityName}",
-            modifier = Modifier.padding(16.dp)
-        )
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(16.dp),
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Column(
+                modifier = Modifier.weight(1f)
+            ) {
+                Text(
+                    text = weather.cityName,
+                    style = MaterialTheme.typography.titleLarge
+                )
+
+                Row(
+                    verticalAlignment = Alignment.CenterVertically
+                ) {
+                    // TODO: Sostituire con un'icona dinamica
+                    Icon(
+                        imageVector = Icons.Default.Star,
+                        contentDescription = "Weather Icon",
+                        modifier = Modifier.size(32.dp)
+                    )
+                    Spacer(modifier = Modifier.width(8.dp))
+                    Text(
+                        text = "${weather.temperature}°C",
+                        style = MaterialTheme.typography.headlineMedium
+                    )
+                }
+            }
+            Icon(
+                imageVector = Icons.AutoMirrored.Filled.KeyboardArrowRight,
+                contentDescription = "Go to details",
+                modifier = Modifier.size(32.dp)
+            )
+        }
     }
 }
 

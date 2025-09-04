471a269447d73996c62bf226567701ca45237a19
nick_2002b <siriusdark95@gmail.com>
2025-09-04 17:08:50 +0200

    Modift TopAppBar

--- ui/src/main/java/com/unibo/ui/common/MeteoSaverTopAppBar.kt
+++ ui/src/main/java/com/unibo/ui/common/MeteoSaverTopAppBar.kt
@@ -1,42 +1,34 @@
 package com.unibo.ui.compose.common
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Add
 import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.sp
 
-// Usiamo @OptIn per dire che siamo consapevoli di usare API sperimentali di Material 3
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun MeteoSaverAppBar(
     title: String,
-    navigationIcon: @Composable () -> Unit = {},
-    actions: @Composable () -> Unit = {}
 ) {
-    TopAppBar(
-        title = { Text(text = title) },
-        navigationIcon = navigationIcon,
-        actions = { actions() },
-        colors = TopAppBarDefaults.topAppBarColors(
-            containerColor = MaterialTheme.colorScheme.primaryContainer,
-            titleContentColor = MaterialTheme.colorScheme.primary
-        )
+    CenterAlignedTopAppBar(
+    title = { Text(text = title,
+        style = TextStyle(
+            fontSize = 26.sp,
+                    )
+                )
+            },
+    colors = TopAppBarDefaults.topAppBarColors(
+        containerColor = MaterialTheme.colorScheme.primaryContainer,
+        titleContentColor = MaterialTheme.colorScheme.primary
+        ),
     )
 }
 
-// Creiamo anche una preview per la nostra TopAppBar isolata!
+
 @Preview
 @Composable
 fun MeteoSaverAppBarPreview() {
         MeteoSaverAppBar(
             title = "MeteoSaver",
-            actions = {
-                IconButton(onClick = { }) {
-                    Icon(
-                        imageVector = Icons.Default.Add,
-                        contentDescription = "Add Icon"
-                    )
-                }
-            }
         )
     }

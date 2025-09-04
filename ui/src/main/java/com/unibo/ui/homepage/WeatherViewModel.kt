4596aad0b46e0ea171cf39c897009e5a4469380f
nick_2002b <siriusdark95@gmail.com>
2025-09-04 18:15:14 +0200

    Create a fun to use refreshAllCitiesUseCase

--- ui/src/main/java/com/unibo/ui/homepage/WeatherViewModel.kt
+++ ui/src/main/java/com/unibo/ui/homepage/WeatherViewModel.kt
@@ -5,13 +5,15 @@ import androidx.lifecycle.viewModelScope
 import com.unibo.domain.model.Weather
 import com.unibo.domain.usecases.FetchRemoteWeatherUseCase
 import com.unibo.domain.usecases.GetWeatherListUseCase
+import com.unibo.domain.usecases.RefreshAllCitiesUseCase
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.launch
 
 class WeatherViewModel (
     private val getWeatherListUseCase: GetWeatherListUseCase,
-    private val fetchRemoteWeatherUseCase: FetchRemoteWeatherUseCase
+    private val fetchRemoteWeatherUseCase: FetchRemoteWeatherUseCase,
+    private val refreshAllCitiesUseCase: RefreshAllCitiesUseCase
 ): ViewModel() {
 
     private val _weatherList = MutableStateFlow<List<Weather>>(listOf())
@@ -27,7 +29,9 @@ class WeatherViewModel (
     fun searchCity(cityName: String){
         fetchRemoteWeatherUseCase.invoke(cityName)
     }
-
+    fun onRefresh(){
+        refreshAllCitiesUseCase()
+    }
     private fun observeWeatherList(){
         viewModelScope.launch {
             _showLoader.value = true
@@ -38,5 +42,4 @@ class WeatherViewModel (
             }
         }
     }
-
 }

cc79b489ac5a284140ae346f333fef0590000c24
nick_2002b <siriusdark95@gmail.com>
2025-09-04 18:16:16 +0200

    Update ViewModelFactory

--- ui/src/main/java/com/unibo/ui/homepage/WeatherViewModelFactory.kt
+++ ui/src/main/java/com/unibo/ui/homepage/WeatherViewModelFactory.kt
@@ -4,17 +4,20 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
 import com.unibo.domain.usecases.FetchRemoteWeatherUseCase
 import com.unibo.domain.usecases.GetWeatherListUseCase
+import com.unibo.domain.usecases.RefreshAllCitiesUseCase
 
 
 class WeatherViewModelFactory (
     private val getWeather: GetWeatherListUseCase,
     private val fetchRemoteWeather: FetchRemoteWeatherUseCase
+    private val refreshAllCitiesUseCase: RefreshAllCitiesUseCase
 ): ViewModelProvider.Factory {
     override fun <T : ViewModel> create(modelClass: Class<T>): T {
         if (modelClass.isAssignableFrom(WeatherViewModel::class.java)) {
             return WeatherViewModel(
                 getWeather,
-                fetchRemoteWeather
+                fetchRemoteWeather,
+                refreshAllCitiesUseCase
             ) as T
         }
         throw IllegalArgumentException("Unknown ViewModel class")

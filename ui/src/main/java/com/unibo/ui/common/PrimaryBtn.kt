30c2ab6817c5926f71611d45048890c6e0cb0b9a
nick_2002b <siriusdark95@gmail.com>
2025-09-04 17:56:00 +0200

    Add new parameter to PrimaryBtn

--- ui/src/main/java/com/unibo/ui/common/PrimaryBtn.kt
+++ ui/src/main/java/com/unibo/ui/common/PrimaryBtn.kt
@@ -3,15 +3,26 @@ package com.unibo.ui.common
 import androidx.compose.material3.ElevatedButton
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.TextUnit
+import androidx.compose.ui.unit.sp
 
 @Composable
 fun PrimaryBtn (
     btnName: String,
     onBtnClick: () -> Unit,
+    modifier: Modifier = Modifier,
+    fontSize: TextUnit = 20.sp
 ) {
-    ElevatedButton(onClick = {onBtnClick() }) {
-        Text(btnName)
+    ElevatedButton(
+        onClick = onBtnClick,
+        modifier = modifier,
+    ) {
+        Text(
+            text = btnName,
+            fontSize = fontSize
+        )
     }
 }
 

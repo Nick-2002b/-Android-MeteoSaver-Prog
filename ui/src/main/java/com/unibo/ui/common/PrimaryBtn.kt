fa3b9d17399ab0a27404a9bc4d6103278f655b9f
nick_2002b <siriusdark95@gmail.com>
2025-09-04 17:21:07 +0200

    Implement Btn

--- ui/src/main/java/com/unibo/ui/common/PrimaryBtn.kt
+++ ui/src/main/java/com/unibo/ui/common/PrimaryBtn.kt
@@ -1,7 +1,25 @@
 package com.unibo.ui.common
 
+import androidx.compose.material3.ElevatedButton
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
 
 @Composable
-fun PrimaryBtn {
+fun PrimaryBtn (
+    btnName: String,
+    onBtnClick: () -> Unit,
+) {
+    ElevatedButton(onClick = {onBtnClick() }) {
+        Text(btnName)
+    }
+}
+
+@Preview(showBackground = false)
+@Composable
+fun PrimaryBtnPreview() {
+    PrimaryBtn(
+        btnName = "Primary Button",
+        onBtnClick = {}
+    )
 }

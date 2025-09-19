e5041c1a4f3bcee714a41acf392c1181982639ec
nick_2002b <siriusdark95@gmail.com>
2025-09-19 19:13:07 +0200

    Implement HomeScreen UI

--- ui/src/main/java/com/unibo/ui/homepage/HomeScreen.kt
+++ ui/src/main/java/com/unibo/ui/homepage/HomeScreen.kt
@@ -1,7 +1,142 @@
 package com.unibo.ui.homepage
 
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.pulltorefresh.PullToRefreshContainer
+import androidx.compose.material3.pulltorefresh.rememberPullToRefreshState
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import com.unibo.domain.model.Weather
+import com.unibo.ui.common.PrimaryBtn
+import com.unibo.ui.compose.common.Loader
+import com.unibo.ui.compose.common.MeteoSaverAppBar
+
+
+
 
 @Composable
-fun HomeScreen {
+fun HomeScreen(
+    viewModel: WeatherViewModel,
+    onItemClick: (Weather) -> Unit,
+){
+    val weatherList = viewModel.weatherList.collectAsStateWithLifecycle()
+    val isLoading = viewModel.showLoader.collectAsStateWithLifecycle()
+    HomeScreenLayout(
+        weatherList = weatherList.value,
+        isLoading = isLoading.value,
+        onItemClick = onItemClick,
+        onRefresh = { viewModel.onRefresh()}
+    )
+
+    LaunchedEffect(key1 = Unit) {
+        viewModel.searchCity(cityName = "Milano")
+    }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun HomeScreenLayout(
+    weatherList: List<Weather>,
+    isLoading: Boolean,
+    onItemClick: (Weather) -> Unit,
+    onRefresh: () -> Unit
+) {
+    val pullToRefreshState = rememberPullToRefreshState()
+
+    Column(modifier = Modifier.fillMaxSize()) {
+
+        MeteoSaverAppBar(
+            title = "MeteoSaver",
+        )
+        PrimaryBtn(
+            "Aggiungi Città",
+            onBtnClick = {},
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(16.dp),
+        )
+        if (isLoading) {
+            Loader()
+        }
+        LazyColumn(
+            modifier = Modifier
+                .fillMaxSize(),
+            contentPadding = PaddingValues(16.dp),
+            verticalArrangement = Arrangement.spacedBy(10.dp)
+
+        ) {
+            items(weatherList) { weather ->
+                WeatherCard(
+                    weather = weather,
+                    onItemClick = onItemClick,
+                )
+            }
+        }
+    }
+}
+
+@Preview(showBackground = true, widthDp = 360, heightDp = 640)
+@Composable
+fun HomeScreenPreview() {
+    val fakeWeatherList = listOf(
+        Weather(
+            cityName = "Milano",
+            temperature = 27.0,
+            weatherDescription = "Sereno",
+            icon = "",
+            humidity = 55.0,
+            windSpeed = 12.0,
+            feelsLike = 29.0
+        ),
+        Weather(
+            cityName = "Roma",
+            temperature = 19.0,
+            weatherDescription = "Pioggia",
+            icon = "",
+            humidity = 80.0,
+            windSpeed = 20.0,
+            feelsLike = 18.0
+        ),
+        Weather(
+            cityName = "Torino",
+            temperature = 29.0,
+            weatherDescription = "Pioggia",
+            icon = "",
+            humidity = 80.0,
+            windSpeed = 20.0,
+            feelsLike = 18.0
+        )
+    )
+
+    HomeScreenLayout(
+            weatherList = fakeWeatherList,
+            isLoading = false,
+            onItemClick = {},
+            onRefresh = {}
+        )
+}
+
+
+
+@Preview(showBackground = true, widthDp = 360, heightDp = 640)
+@Composable
+fun HomeScreenLoadingPreview() {
+        HomeScreenLayout(
+            weatherList = emptyList(),
+            isLoading = true,
+            onItemClick = {},
+            onRefresh = {},
+        )
 }

3c7cf4cdaaa2dc8e5e701fed91cf94c8c9894032
nick_2002b <nicolaebalaban@studio.unibo.it>
2025-09-20 17:41:45 +0200

    add refresh button to the UI

--- ui/src/main/java/com/unibo/ui/homepage/HomeScreen.kt
+++ ui/src/main/java/com/unibo/ui/homepage/HomeScreen.kt
@@ -8,11 +8,16 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Refresh
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.pulltorefresh.rememberPullToRefreshState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
@@ -42,6 +47,7 @@ fun HomeScreen(
     }
 }
 
+// Function to create the HomeScreen layout
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun HomeScreenLayout(
@@ -56,6 +62,14 @@ fun HomeScreenLayout(
 
         MeteoSaverAppBar(
             title = "MeteoSaver",
+            actions = {
+                IconButton(onClick = onRefresh) {
+                    Icon(
+                        imageVector = Icons.Default.Refresh,
+                        contentDescription = "Aggiorna dati meteo",
+                    )
+                }
+            }
         )
         PrimaryBtn(
             "Aggiungi Città",
@@ -64,7 +78,7 @@ fun HomeScreenLayout(
                 .fillMaxWidth()
                 .padding(16.dp),
         )
-        if (isLoading) {
+        if (isLoading && weatherList.isEmpty()) {
             Loader()
         }
         LazyColumn(

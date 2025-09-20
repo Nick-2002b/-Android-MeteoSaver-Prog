a59cd6d38f7bfd8cbc846ca93ef3c0792c16bba7
nick_2002b <nicolaebalaban@studio.unibo.it>
2025-09-20 19:40:10 +0200

    Update PrimaryBtn UI component

--- ui/src/main/java/com/unibo/ui/common/PrimaryBtn.kt
+++ ui/src/main/java/com/unibo/ui/common/PrimaryBtn.kt
@@ -1,9 +1,12 @@
 package com.unibo.ui.common
 
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.ElevatedButton
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.TextUnit
 import androidx.compose.ui.unit.sp
@@ -13,11 +16,17 @@ fun PrimaryBtn (
     btnName: String,
     onBtnClick: () -> Unit,
     modifier: Modifier = Modifier,
-    fontSize: TextUnit = 20.sp
+    fontSize: TextUnit = 20.sp,
+    backgroundColor: Color = MaterialTheme.colorScheme.surfaceContainerLow,
+    contentColor: Color = MaterialTheme.colorScheme.primary
 ) {
     ElevatedButton(
         onClick = onBtnClick,
         modifier = modifier,
+        colors = ButtonDefaults.buttonColors(
+            containerColor = backgroundColor,
+            contentColor = contentColor
+        )
     ) {
         Text(
             text = btnName,

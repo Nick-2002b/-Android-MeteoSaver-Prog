56f124d13f5f4b47847ae3ae5e12c794a0cb575d
nick_2002b <nicolaebalaban@studio.unibo.it>
2025-09-21 19:29:20 +0200

    add coil-compose library

--- ui/build.gradle.kts
+++ ui/build.gradle.kts
@@ -59,4 +59,5 @@ dependencies {
     androidTestImplementation(libs.androidx.espresso.core)
     implementation(libs.kotlinx.serialization.json)
     implementation(libs.androidx.navigation.compose)
+    implementation("io.coil-kt:coil-compose:2.6.0")
 }

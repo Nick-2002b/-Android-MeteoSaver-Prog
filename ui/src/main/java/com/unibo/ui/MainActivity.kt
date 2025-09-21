0e95df02affadf323c4641de8a4442df3bb34621
nick_2002b <nicolaebalaban@studio.unibo.it>
2025-09-21 19:25:58 +0200

    Implement the main Activity

--- ui/src/main/java/com/unibo/ui/MainActivity.kt
+++ ui/src/main/java/com/unibo/ui/MainActivity.kt
@@ -1,7 +1,14 @@
 package com.unibo.ui
 
+import android.os.Bundle
 import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
 
 class MainActivity: ComponentActivity() {
-
+    override fun onCreate(savedIstanceState: Bundle?) {
+        super.onCreate(savedIstanceState)
+        setContent {
+            NavigationHost()
+        }
+    }
 }

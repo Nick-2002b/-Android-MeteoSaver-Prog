1d26463253cc6ce1b6d3fb46436cdb5b0c19df80
nick_2002b <nicolaebalaban@studio.unibo.it>
2025-09-21 16:53:37 +0200

    Update Weather model

--- domain/src/main/java/com/unibo/domain/model/Weather.kt
+++ domain/src/main/java/com/unibo/domain/model/Weather.kt
@@ -5,6 +5,8 @@ data class Weather (
     val icon: String,
     val weatherDescription: String,
     val temperature: Double,
+    val tempMin: Double,
+    val tempMax: Double,
     val humidity: Double,
     val windSpeed: Double,
     val feelsLike: Double,

978dd162ab86a2cc5952f92e2991de9eec76c1f3
nick_2002b <nicolaebalaban@studio.unibo.it>
2025-09-21 18:21:24 +0200

    Create NavigationHost file

---
+++ ui/src/main/java/com/unibo/ui/NavigationHost.kt
@@ -0,0 +1,9 @@
+package com.unibo.ui
+
+import androidx.compose.runtime.Composable
+import androidx.navigation.compose.rememberNavController
+
+@Composable
+fun NavigationHost () {
+    val navController = rememberNavController()
+}

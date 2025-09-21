86c8af9d0cbd918ca7f2e31d0e42a7cc282809f3
nick_2002b <nicolaebalaban@studio.unibo.it>
2025-09-22 00:35:02 +0200

    Update ModelFactory for the new deleteCityUseCase

--- ui/src/main/java/com/unibo/ui/screens/homepage/WeatherViewModelFactory.kt
+++ ui/src/main/java/com/unibo/ui/screens/homepage/WeatherViewModelFactory.kt
@@ -2,6 +2,7 @@ package com.unibo.ui.screens.homepage
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
+import com.unibo.domain.usecases.DeleteCityUseCase
 import com.unibo.domain.usecases.FetchRemoteWeatherUseCase
 import com.unibo.domain.usecases.GetWeatherListUseCase
 import com.unibo.domain.usecases.RefreshAllCitiesUseCase
@@ -10,14 +11,16 @@ import com.unibo.domain.usecases.RefreshAllCitiesUseCase
 class WeatherViewModelFactory (
     private val getWeather: GetWeatherListUseCase,
     private val fetchRemoteWeather: FetchRemoteWeatherUseCase,
-    private val refreshAllCitiesUseCase: RefreshAllCitiesUseCase
+    private val refreshAllCitiesUseCase: RefreshAllCitiesUseCase,
+    private val deleteCityUseCase: DeleteCityUseCase
 ): ViewModelProvider.Factory {
     override fun <T : ViewModel> create(modelClass: Class<T>): T {
         if (modelClass.isAssignableFrom(WeatherViewModel::class.java)) {
             return WeatherViewModel(
                 getWeather,
                 fetchRemoteWeather,
-                refreshAllCitiesUseCase
+                refreshAllCitiesUseCase,
+                deleteCityUseCase
             ) as T
         }
         throw IllegalArgumentException("Unknown ViewModel class")

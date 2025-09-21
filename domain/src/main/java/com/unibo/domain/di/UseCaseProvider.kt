dc90d5db7217408a34d010127494039eda008bf6
nick_2002b <nicolaebalaban@studio.unibo.it>
2025-09-21 19:52:52 +0200

    update the caseprovider

--- domain/src/main/java/com/unibo/domain/di/UseCaseProvider.kt
+++ domain/src/main/java/com/unibo/domain/di/UseCaseProvider.kt
@@ -1,5 +1,7 @@
 package com.unibo.domain.di
 
+import com.unibo.domain.usecases.DeleteCityUseCase
+import com.unibo.domain.usecases.DeleteCityUseCaseImpl
 import com.unibo.domain.usecases.FetchRemoteWeatherUseCase
 import com.unibo.domain.usecases.FetchRemoteWeatherUseCaseImpl
 import com.unibo.domain.usecases.GetWeatherListUseCase
@@ -11,17 +13,20 @@ object UseCaseProvider {
     lateinit var fetchRemoteWeatherUseCase: FetchRemoteWeatherUseCase
     lateinit var getWeatherListUseCase: GetWeatherListUseCase
     lateinit var refreshAllCitiesUseCase: RefreshAllCitiesUseCase
+    lateinit var deleteCityUseCase: DeleteCityUseCase
     fun setup(
         repositoryProvider: RepositoryProvider,
     ){
         fetchRemoteWeatherUseCase = FetchRemoteWeatherUseCaseImpl(
             repository = repositoryProvider.weatherRepository
         )
-
         getWeatherListUseCase = GetWeatherListUseCaseImpl(
             repository = repositoryProvider.weatherRepository
         )
         refreshAllCitiesUseCase = RefreshAllCitiesUseCaseImpl(
+            repository = repositoryProvider.weatherRepository
+        )
+        deleteCityUseCase = DeleteCityUseCaseImpl(
             repository = repositoryProvider.weatherRepository
         )
 

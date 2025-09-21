dcbe5eb72a074f20494a544be5c87685bcf1284e
nick_2002b <nicolaebalaban@studio.unibo.it>
2025-09-21 16:55:42 +0200

    Update moshi response Model

--- data/src/main/java/com/unibo/data/remote/models/Main.kt
+++ data/src/main/java/com/unibo/data/remote/models/Main.kt
@@ -8,4 +8,6 @@ data class Main(
     @Json(name = "temp") val temperature: Double? = null,
     @Json(name = "humidity") val humidity: Double? = null,
     @Json(name = "feels_like") val feelsLike: Double? = null,
+    @Json(name = "temp_min") val tempMin: Double? = null,
+    @Json(name = "temp_max") val tempMax: Double? = null,
 )

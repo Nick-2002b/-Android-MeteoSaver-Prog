ed6c639cec2209ceaa1b0a80ec9b594416d35550
nick_2002b <nicolaebalaban@studio.unibo.it>
2025-10-02 18:11:00 +0200

    Fix Delete Btn disappear in landscape

--- ui/src/main/java/com/unibo/ui/screens/details/DescriptionScreen.kt
+++ ui/src/main/java/com/unibo/ui/screens/details/DescriptionScreen.kt
@@ -12,7 +12,9 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.ArrowBack
 import androidx.compose.material3.Icon
@@ -85,7 +87,8 @@ fun DescriptionScreenLayout(
         Column (
             modifier = Modifier
                 .fillMaxSize()
-                .padding(16.dp),
+                .padding(16.dp)
+                .verticalScroll(rememberScrollState()),
             horizontalAlignment = Alignment.CenterHorizontally,
         ) {
             Spacer(modifier = Modifier.weight(0.5f))
